85f3e6b616f489cfd140534ae969b06de7d22045
murgrona <sawejonna@gmail.com>
2021-11-18 14:58:22 +0100

    Lagt till
    Början på delete i rap_name

---
+++ rap_name/delete.php
@@ -0,0 +1,48 @@
+<?php
+error_reporting(-1);
+
+require_once "../functions.php";
+
+// Hämta metoden
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+$rappers = loadJson("../rap_name.json");
+
+// Hämta Json data
+$data = file_get_contents("php://input");
+$requestData = json_decode($data, true);
+
+//Radera baserat på id
+if ($requestMethod === "DELETE") {
+    if (!isset($requestData["id"])) {
+        sendJson(
+            [
+            "code" => 1,
+            "message" => "missing id"
+            ],
+            400
+        );
+    }
+    $id = $requestData["id"];
+    $found = false;
+    // Går igenom idn och tar bort
+    foreach($rappers as $index => $rapper){
+    if($rapper["id"] === $id) {
+        $found = true;
+        array_splice($rappers, $index, 1);
+        break;
+        }
+    }
+    if ($found === false) {
+        sendJson(
+            [
+                "code" => 2,
+                "message" => "The rapper by `id` does not exist"
+            ],
+            404
+        );
+    }
+    // Uppdaterar filen
+    saveJson("../rap_name.json", $rappers);
+    sendJson(["id" => $id]);
+};
+?>
--- record_company/delete.php
+++ record_company/delete.php
@@ -5,6 +5,7 @@ require_once "../functions.php";
 
 // Hämta metoden
 $requestMethod = $_SERVER["REQUEST_METHOD"];
+$records = loadJson("../record_company.json");
 
 // Hämta Json data
 $data = file_get_contents("php://input");
@@ -22,31 +23,26 @@ if ($requestMethod === "DELETE") {
         );
     }
     $id = $requestData["id"];
-
+    $found = false;
     // Går igenom idn och tar bort
-    foreach($recordsDecode as $index => $user){
-    if($user["id"] === $id) {
-        //$found = true;
-        array_splice($recordsDecode, $index, 1);
+    foreach($records as $index => $record){
+    if($record["id"] === $id) {
+        $found = true;
+        array_splice($records, $index, 1);
         break;
         }
     }
-
+    if ($found === false) {
+        sendJson(
+            [
+                "code" => 2,
+                "message" => "The recor company by `id` does not exist"
+            ],
+            404
+        );
+    }
     // Uppdaterar filen
-    $json = json_encode($index, JSON_PRETTY_PRINT);
-    file_put_contents("../record_company.json", $json);
+    saveJson("../record_company.json", $records);
     sendJson(["id" => $id]);
 };
-/*$getRecord = file_get_contents("../record_company.json");
-$recordsDecode = json_decode($getRecord, true);
-
-
-$found = false;
-
-if($requestMethod == "DELETE") {
-    
-    
-    saveJson("../record_company.json", $recordsDecode);
-    sendJson(["id" => $id]);
-}*/
 ?>
