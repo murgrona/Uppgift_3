f6f8eeddbdf396827af6327455137198d63119b5
murgrona <sawejonna@gmail.com>
2021-11-19 14:36:03 +0100

    Update read.php

--- rap_name/read.php
+++ rap_name/read.php
@@ -1,12 +1,24 @@
 <?php
+error_reporting(-1);
+
 require_once "../functions.php";
 
 // Hämta metoden
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 $rappers = loadJson("../rap_name.json");
 
-
 if ($requestMethod === "GET") {
+    //Hämta rappare med egen nyckel (titles)
+    if (isset($_GET["titles"])) {
+        $rapTitle = explode(",",$_GET["titles"]);
+        $titleArray = [];
+        foreach ($rappers as $rapT) {
+            if (in_array($rapT["title"], $rapTitle)) {
+                $titleArray[] = $rapT;
+            }
+        }
+        sendJson($titleArray);
+    }
     //Hämta begränsat antal rappare
     if (isset($_GET["limit"])) {
         $limit = $_GET["limit"];
@@ -17,7 +29,6 @@ if ($requestMethod === "GET") {
     if (isset($_GET["ids"])) {
         $ids = explode(",",$_GET["ids"]);
         $rappersId = [];
-
         foreach ($rappers as $rapper) {
             if (in_array($rapper["id"], $ids)) {
                 $rappersId[] = $rapper;
